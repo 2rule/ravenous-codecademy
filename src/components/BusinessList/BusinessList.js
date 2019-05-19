4d67eebc9d7a800cc1c647fa78c125ad441fd739
2rule <atugruly@gmail.com>
2019-05-19 22:19:08 +0200

    Before the 3rd part

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,13 +6,13 @@ import Business from '../Business/Business';
 class BusinessList extends React.Component {
   render() {
     return (
-      <div className="BusinessList">
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-</div>
+      <div className="BusinessList" businesses={this.props.businesses}>
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business}></Business>
+          })
+        }
+      </div>
     );
   }
 }
